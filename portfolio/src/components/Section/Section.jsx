5c0b668761711127062752e11ef11025b5a5d052
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-27 15:52:19 -0500

    creating project section

--- portfolio/src/components/Section/Section.jsx
+++ portfolio/src/components/Section/Section.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Divspl from './DivSplash.png'
+import Shit from './shit.png'
 import "./section.css"
 import { motion } from "framer-motion"
 import { useScroll } from 'framer-motion'
@@ -8,16 +8,7 @@ const Section = () => {
   return (
     <div className="cards">
         <div className="card">
-            <img className = "divspal h-[50vh]" src= {Divspl} alt="" />
-        </div>
-        <div className="card">
-            <img className = "divspal h-[50vh]" src= {Divspl} alt="" />
-        </div>
-        <div className="card">
-            <img className = "divspal h-[50vh]" src= {Divspl} alt="" />
-        </div>
-        <div className="card">
-            <img className = "divspal h-[50vh]" src= {Divspl} alt="" />
+            <img className = "divspal h-[50vh]" src= {Shit} alt="" />
         </div>
     </div>
   )
