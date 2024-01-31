ef87ab37ca4109f87afbe2617a994736bcbc3f8f
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-31 12:21:13 -0500

    Diffpagelayout

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import "./navbar.css"
 import Pixvelp from "./PXELVELP.png"
 import { motion } from "framer-motion"
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
 
-    <div className="container inline-flex items-center justify-between">
+    <div className="seperator inline-flex items-center justify-between">
         <div className="logo">
           <h1 className='fontlad text-3xl font-extrabold'><span style={{ color: '#6366F1' }}>&lt;</span>velp<span style={{ color: '#6366F1' }}>/&gt;</span></h1>
         </div>
         <div className="items">
             <ul className='inline-flex items-center gap-5'>
-                <li><a href = "#">Projects</a></li>
+                  <Link to = "/home">
+                    <button>Projects</button>
+                  </Link>
                 <li><a>My Shop</a></li>
-                <li><button>Blog</button></li>
+                  <Link to = "/blog">
+                    <button>Blog</button>
+                  </Link>
                 <li><a>About</a></li>
                 <li>
                 <motion.div className = 
--- portfolio/src/pages/Blog.jsx
+++ portfolio/src/pages/Blog.jsx
@@ -1,14 +1,45 @@
 import Navbar from "../components/Navbar/Navbar";
-
+import Products from "../components/Products/Products";
+import "./blogs.css"
 
 const Blog = () => {
 
     return (
-
-        <>
+        
+        <div>
             <Navbar />
-            <h3>Blogsite</h3>
-        </>
+
+            <div className="headtitle mt-20">
+
+                <div className="leftsec">
+                <h1 className="curious text-6xl font-semibold">Exploring my curiosities.</h1>
+
+
+                </div>
+                <div className="rightsec">
+                <h1 className="curious text-4xl font-semibold">Diving deeper into lifestyle, code, design, writing.</h1>
+                </div>
+
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+
+            <Products className = "poraguloo"/>
+
+
+        </div>
 
     )
 
