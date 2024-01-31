f63210da9b4357a871a6a6f058518cbf3ddb12d6
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-30 20:17:46 -0500

    blogsite

---
+++ portfolio/src/pages/Blog.jsx
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar/Navbar";
+
+
+const Blog = () => {
+
+    return (
+
+        <>
+
+            <Navbar />
+            <h3>Blogsite</h3>
+        
+        
+        </>
+
+
+    )
+
+
+
+}
---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar/Navbar";
+
+
+const Home = () =>{
+
+    return (
+
+        <>
+        
+            <Navbar />
+            <h2>Home Page</h2>
+        
+        </>
+
+    )
+
+}
+
+
+export default Home;
