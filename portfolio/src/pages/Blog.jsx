82c93361cef2f8162a491a0a4cb610145cebaee5
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-01-31 12:48:21 -0500

    how do we get there

--- portfolio/src/pages/Blog.jsx
+++ portfolio/src/pages/Blog.jsx
@@ -9,18 +9,39 @@ const Blog = () => {
         <div>
             <Navbar />
 
-            <div className="headtitle mt-20">
+            <div className="headtitle mt-[15vh] max-w-[200vh]">
 
                 <div className="leftsec">
-                <h1 className="curious text-6xl font-semibold">Exploring my curiosities.</h1>
+                <h1 className="curious text-6xl font-semibold"><span style={{ color: '#6366F1' }}>&lt;</span> Combining my curiosities.</h1>
 
 
                 </div>
                 <div className="rightsec">
-                <h1 className="curious text-4xl font-semibold">Diving deeper into lifestyle, code, design, writing.</h1>
+                <h1 className="curious text-3xl font-semibold">Diving deeper into lifestyle, code, design, writing. <span style={{ color: '#6366F1' }}>/&gt;</span></h1>
                 </div>
 
             </div>
+
+            <div className="articleposts mt-20 cursor-pointer space-y-10">
+                <div className="artone">
+                    <div className="date">2024-01-31</div>
+                    <div className="articlename font-semibold underline">Understanding how to get to that planet 🪐</div>
+
+                </div>
+                <div className="artone">
+                    <div className="date">2024-01-31</div>
+                    <div className="articlename font-semibold underline">Thoughts on stacking skills, creating passions.</div>
+                    <div className="bubbles flex gap-2">
+                    <div className="bubble font-bold text-xs mt-2">
+                        Thoughts
+                    </div>
+                    <div className="bubble font-bold text-xs mt-2">
+                        Nocode
+                    </div>
+                    </div>
+                </div>
+            </div>
+
             <br />
             <br />
             <br />
