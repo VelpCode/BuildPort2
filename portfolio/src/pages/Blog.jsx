3205f9b8c6f5da47fc41d0d3dd8a9bb45adc34d9
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-06 14:14:04 -0500

    Creating blog classes

--- portfolio/src/pages/Blog.jsx
+++ portfolio/src/pages/Blog.jsx
@@ -8,19 +8,51 @@ import { Link } from "react-router-dom";
 
 const Blog = () => {
 
+
+    const blogs = [
+
+        {
+            "id":1,
+            "title": 'Blog 1',
+            'desc': 'Talking about how to travel to that magical planet',
+            'coverImg': 'https://s3-alpha.figma.com/hub/file/1164680562/95e16f73-b877-4d64-8f0e-0d1d652782ea-cover.png'
+        },
+        {
+            "id":2,
+            "title": 'Blog 2',
+            'desc': 'Talking about how to travel to that magical planet',
+            'coverImg': 'https://s3-alpha.figma.com/hub/file/1164680562/95e16f73-b877-4d64-8f0e-0d1d652782ea-cover.png'
+        },
+        {
+            "id":1,
+            "title": 'Blog 3',
+            'desc': 'Talking about how to travel to that magical planet',
+            'coverImg': 'https://s3-alpha.figma.com/hub/file/1164680562/95e16f73-b877-4d64-8f0e-0d1d652782ea-cover.png'
+        }
+
+    ]
+
     return (
         
         <div className="w-full">
             <Navbar />
         <div className="max-w-[1240px] mx-auto my-20">
             <div className="grid lg:grid-cols-3 gap-8 px-4 text-black">
-                <div className="bg-white rounded-xl overflow-hidden drop-shadow-md">
-                    <img className="h-56 w-full object-cover" src={"https://s3-alpha.figma.com/hub/file/1164680562/95e16f73-b877-4d64-8f0e-0d1d652782ea-cover.png"} />
+                
+                
+                {blogs.map((blog) => 
+
+                    <div className="bg-white rounded-xl overflow-hidden drop-shadow-md">
+                    <img className="h-56 w-full object-cover" src={blog.coverImg} />
                     <div className="p-8">
-                        <div className="font-bold text-2xl my-1">Travelling to saturn</div>
-                        <div className="text-sm text-gray-600 my-2">Talking about the differences in</div>
+                            <div className="font-bold text-2xl my-1">{blog.title}</div>
+                            <div className="text-sm text-gray-600 my-2">{blog.desc}</div>
+                        </div>
                     </div>
-                </div>
+                
+                )}
+
+                
             </div>
         </div>
         </div>
