22a3580c7348e279b98b42341358013430715ceb
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-07 14:33:07 -0500

    creating blog content section

--- portfolio/src/components/Blogcontent/Blogcontent.jsx
+++ portfolio/src/components/Blogcontent/Blogcontent.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+
 const Blogcontent = () => {
   return (
-    <div>Blogcontent</div>
+    <div>
+      <div className = "w-full pb">
+        <div className = "max-w-[1240px] mx-auto">
+          <div className='grid lg:grid-cols-3 md:grid-cols-3 ss:gri-cols-1 gap-x-8 gap-y-8 px-4 sm:pt-20 md:mt-0 ss: pt-20 text-black'>
+
+            <div className = "col-span-2 gap-x-8 gap-y-8">
+              <img className='h-30 object-cover' src = {"kdakdjas"} />
+              <h1 className='font-bold text-6xl my-1 pt-5'>Blog Title.</h1>
+              <div className='pt-5 '><p>This is the blog content.</p></div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
