b23e0f01e8083eb440156acb5ee6fd0194aa4e95
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-10 18:07:30 -0500

    chaning up product sec

--- portfolio/src/components/Products/Products.jsx
+++ portfolio/src/components/Products/Products.jsx
@@ -11,56 +11,97 @@ const Products = () => {
             <h1 className='t text-4xl font-semibold'><span style={{ color: '#6366F1' }}>&lt;</span>Products</h1>
             <h1 className='lambo text-xl mb-5'>Check out the products/templates I've created, all designed to help you with your goals.<span className= "text-3xl font-bold" style={{ color: '#6366F1' }}>/&gt;</span></h1>    
         </div>
-        <div className="productbox flex justify-between gap-[10vh] align-middle items-center">
-            <div className="prodone">
-                <img className = " h-[40vh] w-[50vh]" src= {JavaScr} alt="" />
-                <div className="bubbles flex gap-5 mt-3">
-                    <div className="bubble font-bold text-sm">
-                        Coding
+
+        <div className="max-w-[1240px] mx-auto my-[8vh]">
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8 px-4 text-black">
+                
+                
+               
+                    <div className="bg-white rounded-xl overflow-hidden drop-shadow-lg">
+                    <img className="" src={JavaScr}/>
+                    <div className="p-8">
+                            <div className="font-bold text-2xl my-1">JS Concept Dashboard</div>
+                            <div className="babble text-xs font-thin max-xs mt-2">Code</div>
+                            <div className="text-sm text-gray-600 my-2 ml-1 mt-4">JavaScript dashboard with 60 different concepts & notes</div>
+                            <div className="read text-xs font-semibold ml-1 mt-3">$0</div>
+                        </div>
                     </div>
-                    <div className="bubble font-bold text-sm">
-                        Learning
+                    <div className="bg-white rounded-xl overflow-hidden drop-shadow-lg">
+                    <img className="" src={JavaScr}/>
+                    <div className="p-8">
+                            <div className="font-bold text-2xl my-1">Daily Tracker</div>
+                            <div className="babble text-xs font-thin max-xs mt-2">Systems</div>
+                            <div className="text-sm text-gray-600 my-2 ml-1 mt-4">JavaScript dashboard with 60 different concepts & notes</div>
+                            <div className="read text-xs font-semibold ml-1 mt-3">$0</div>
+                        </div>
                     </div>
-                </div>
-                <div className="desc mt-5">
-                    <div className="cla text-xl font-semibold">JavaScript Notes</div>
-                    <div className="clb max-w-[50vh]">JavaScript dashboard with 60 different concepts & notes</div>
-                </div>
-                <div className="bbox max-w-[10vh] mt-3 text-center font-bold">FREE!</div>
+                    <div className="bg-white rounded-xl overflow-hidden drop-shadow-lg">
+                    <img className="" src={JavaScr}/>
+                    <div className="p-8">
+                            <div className="font-bold text-2xl my-1">Design Bible</div>
+                            <div className="babble text-xs font-thin max-xs mt-2">Design</div>
+                            <div className="text-sm text-gray-600 my-2 ml-1 mt-4">JavaScript dashboard with 60 different concepts & notes</div>
+                            <div className="read text-xs font-semibold ml-1 mt-3">$0</div>
+                        </div>
+                    </div>
+
+                
             </div>
-            <div className="prodone">
-                <img className = " h-[40vh] w-[50vh]" src= {JavaScr} alt="" />
-                <div className="bubbles flex gap-5 mt-3">
-                    <div className="bubble font-bold text-sm">
-                        Coding
-                    </div>
-                    <div className="bubble font-bold text-sm">
-                        Learning
-                    </div>
-                </div>
-                <div className="desc mt-5">
-                    <div className="cla text-xl font-semibold">JavaScript Notes</div>
-                    <div className="clb max-w-[50vh]">JavaScript dashboard with 60 different concepts & notes</div>
-                </div>
-                <div className="bbox max-w-[10vh] mt-3 text-center font-bold">FREE!</div>
-            </div>
-            <div className="prodone">
-                <img className = " h-[40vh] w-[50vh]" src= {JavaScr} alt="" />
-                <div className="bubbles flex gap-5 mt-3">
-                    <div className="bubble font-bold text-sm">
-                        Coding
-                    </div>
-                    <div className="bubble font-bold text-sm">
-                        Learning
-                    </div>
-                </div>
-                <div className="desc mt-5">
-                    <div className="cla text-xl font-semibold">JavaScript Notes</div>
-                    <div className="clb max-w-[50vh]">JavaScript dashboard with 60 different concepts & notes</div>
-                </div>
-                <div className="bbox max-w-[10vh] mt-3 text-center font-bold">FREE!</div>
-            </div>
+            
         </div>
+
+
+
+       {/*  <div className="productbox flex justify-between gap-[10vh] align-middle items-center">
+            <div className="prodone">
+                <img className = " h-[40vh] w-[50vh]" src= {JavaScr} alt="" />
+                <div className="bubbles flex gap-5 mt-3">
+                    <div className="bubble font-bold text-sm">
+                        Coding
+                    </div>
+                    <div className="bubble font-bold text-sm">
+                        Learning
+                    </div>
+                </div>
+                <div className="desc mt-5">
+                    <div className="cla text-xl font-semibold">JavaScript Notes</div>
+                    <div className="clb max-w-[50vh]">JavaScript dashboard with 60 different concepts & notes</div>
+                </div>
+                <div className="bbox max-w-[10vh] mt-3 text-center font-bold">FREE!</div>
+            </div>
+            <div className="prodone">
+                <img className = " h-[40vh] w-[50vh]" src= {JavaScr} alt="" />
+                <div className="bubbles flex gap-5 mt-3">
+                    <div className="bubble font-bold text-sm">
+                        Coding
+                    </div>
+                    <div className="bubble font-bold text-sm">
+                        Learning
+                    </div>
+                </div>
+                <div className="desc mt-5">
+                    <div className="cla text-xl font-semibold">JavaScript Notes</div>
+                    <div className="clb max-w-[50vh]">JavaScript dashboard with 60 different concepts & notes</div>
+                </div>
+                <div className="bbox max-w-[10vh] mt-3 text-center font-bold">FREE!</div>
+            </div>
+            <div className="prodone">
+                <img className = " h-[40vh] w-[50vh]" src= {JavaScr} alt="" />
+                <div className="bubbles flex gap-5 mt-3">
+                    <div className="bubble font-bold text-sm">
+                        Coding
+                    </div>
+                    <div className="bubble font-bold text-sm">
+                        Learning
+                    </div>
+                </div>
+                <div className="desc mt-5">
+                    <div className="cla text-xl font-semibold">JavaScript Notes</div>
+                    <div className="clb max-w-[50vh]">JavaScript dashboard with 60 different concepts & notes</div>
+                </div>
+                <div className="bbox max-w-[10vh] mt-3 text-center font-bold">FREE!</div>
+            </div>
+        </div> */}
     </div>
   )
 }
