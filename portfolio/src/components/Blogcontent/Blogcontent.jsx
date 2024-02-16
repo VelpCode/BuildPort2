d8c9410b4e4b7d9da10489d7ef1fe2f568acaafb
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-02-16 15:47:26 -0500

    WE

--- portfolio/src/components/Blogcontent/Blogcontent.jsx
+++ portfolio/src/components/Blogcontent/Blogcontent.jsx
@@ -15,7 +15,33 @@ const Blogcontent = () => {
         "tags": "VELP",
         'desc': 'what these blogs are going to be about, who am i..',
         'coverImg': 'https://i.ibb.co/tCVH5R6/whoami.png',
-        'content': 'What is up apes. Before I get into it, just want to say that I’m fucking hyped that this is up, and you can see this. The process of building this entire website, from the design, to the branding to the ‘blog’ section and this content you’re currently looking at has been nothing but amazing. As an aspiring developer, designer & writer, I wanted to make this to perfectly encapsulate all of these passions and essentially use this as a lifelong vessel to keep honing on these interests. So thank you. Thank you for reading this, because you were my motivation to build this, and now that it’s up, I’m going to continue writing for me and for you. Who am I? If you don’t follow me on social media or you aren’t a direct friend, you know a thing about me. Velp (Aman Velpula), I’m a 21-year-old university student that has growing up, I was a rebellious that had no interest in school, I was that kid that would doodle on the desks, try, which has led me on a quest to keep improving on my multiple passions, building skills and honestly becoming a modern-day polymath. Who decided that I can only stick to ‘one thing’? Who decided that what am I going to write about? Honestly, I don’t know yet. I want to discover more of that by actually making more posts, writing, and seeing what I like more, but foundationally, I’m going to document my journey'
+        'content': `what is up apes.
+
+        before I get into who I am, I just want to say that I’m honestly hyped that this is up, and you can see this.
+        
+        the process of building this entire website from the design, to the branding to the ‘blog’ section and this content you’re currently looking at has been nothing but amazing.
+        
+        I’ve struggled with 
+        
+        who am I?
+        
+        If you don’t follow me on social media or you aren’t a direct friend, you know a thing about me
+        
+        velp (aman velpula) i’m a 21 year old university student that has 
+        
+        growing up, I was a rebellious that had zero for school, i was the kid that would doodle on the desks, attempt flips from roofs, 
+        
+        my whole life has been taking a bunch of risks, trying a lot of shit out, 
+        
+        which has lead me on a quest to keep experiment, try a bunch of things out and improve the multiple passions I’ve always naturaully stuck to and aim to find a sick way to combine all of it together.
+        
+        **what am i going to write about?**
+        
+        **I** honestly i don’t know yet. I’m using this blog as a means to find out more of what I want to write about, but for now.. I’m going to dive deeper into topics about combining passions, code, design, writing and overall lifestyle.
+        
+        I know this caters to a very niche audience,
+        
+        One step @ a time.`
     },
     {
         "id":2,
