a9333ea5eb86b0069578372441b729ddee6cb9fa
Aman Velpula <89810118+VelpCode@users.noreply.github.com>
2024-03-14 16:28:00 -0400

    redocomit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,17 +4,20 @@ import Hero from "./components/Hero/Hero"
 import Section from './components/Section/Section';
 import Products from './components/Products/Products';
 import Quote from './components/Quote/Quote';
+import Blogpage from './pages/Blogpage';
 import Blogcontent from './pages/Blogcontent';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import About from './pages/About'
-import Blog from './pages/Blog'
 import Article from './pages/Article';
 import useFetch from './hooks/useFetch';
 
 function App() {
 
 
+  let {loading, data, error} = useFetch('http://localhost:1337/api/blogs')
+  if(loading) return <p>Loading..</p>
+  if(error) return <p>Error!</p>
 
   return (
     <div>
@@ -23,7 +26,7 @@ function App() {
           <Route index element ={<Home/>} />
           <Route path = "/home" element = {<Home />} />
           <Route path = "/about" element = {<About />} />
-          <Route path = "/blog" element = {<Blog />} />
+          <Route path = "/blog" element = {<Blogpage blogs = {data?data:""}/>} />
           <Route path = "/article" element = {<Article />} />
           <Route path = "/blogcontent/:id" element = {<Blogcontent />} />
 x         </Routes>
---
+++ portfolio/src/pages/Blogpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Blog from './Blog.jsx'
+
+
+const Blogpage = ({blogs}) => {
+ 
+
+  return (
+    <div>
+        <Blog blogs = {blogs?blogs:""}/>
+    </div>
+  )
+}
+
+export default Blogpage
